fd2cc086371f94be147490be9b506e78c87fb1e4
3tomcha <tomocha.marika@gmail.com>
2018-04-24 09:46:33 +0900

    google text to speechは動かない状況

--- app/src/main/java/site/kobatomo/babigoseisei/MainActivity.java
+++ app/src/main/java/site/kobatomo/babigoseisei/MainActivity.java
@@ -1,29 +1,30 @@
 package site.kobatomo.babigoseisei;
 
-import android.app.Activity;
 import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.ShareCompat;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 import org.json.JSONArray;
-import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
     private EditText text;
     String translatedword="";
+    String targetword="";
 
 
     @Override
@@ -32,23 +33,61 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         Button button = findViewById(R.id.button);
+//        new DownloadFilesTask().execute();
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                targetword = text.getText().toString();
                 new DownloadFilesTask().execute();
             }
         });
+
+        Button twitterbutton = findViewById(R.id.twitterbutton);
+        twitterbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("twitterbutton","twitterbutton");
+                new TwitterShareTask();
+            }
+        });
+
+        Button text2speeech = findViewById(R.id.text2speeech);
+        text2speeech.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txt2speech();
+            }
+        });
+
+
 
         text = findViewById(R.id.text);
         Log.d("translatedword", translatedword);
     }
 
+    //twitter投稿機能
+    private class TwitterShareTask {
+        TwitterShareTask() {
+            ShareCompat.IntentBuilder builder = ShareCompat.IntentBuilder.from(MainActivity.this);
+            builder.setChooserTitle("Choose App");
+            if(translatedword!="") {
+                builder.setText(translatedword);
+            }else{
+                builder.setText(targetword);
+            }
+            builder.setType("text/plain");
+            builder.startChooser();
+        }
+    }
+
+//バビ語変換機能
     private class DownloadFilesTask extends AsyncTask<String,String,String> {
 
 //        APIに接続し、レスポンスを得る
         public String doInBackground(String... params) {
             Log.d("TAG", "doInBackground: ");
-            String targetword = "ごはんたべたい";
+//            String targetword = "ごはんたべたい";
             String urlStr = "http://girly.lolitapunk.jp/babigoapi/index.php?word="+targetword;
             HttpURLConnection con = null;
             InputStream is = null;
@@ -138,5 +177,50 @@ public class MainActivity extends Activity {
         }
         return sb.toString();
     }
+
+
+    private String txt2speech(){
+
+        String urlStr = "https://texttospeech.googleapis.com/v1beta1/text:synthesize";
+        HttpURLConnection con = null;
+        InputStream is = null;
+        String result = "";
+
+        try{
+            URL url = new URL(urlStr);
+            Log.d("URL",url.toString());
+            con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("POST");
+            Log.d("con",con.toString());
+            con.connect();
+            is = con.getInputStream();
+            Log.d("is",is.toString());
+            result = is2String(is);
+            Log.d("result",result.toString());
+        }catch (MalformedURLException ex){
+        }
+        catch(IOException ex) {
+        }
+
+        finally {
+            if(con != null) {
+                con.disconnect();  // （10）
+            }
+            if(is != null) {
+                try {
+                    is.close();  // （11）
+                }
+                catch(IOException ex) {
+                }
+            }
+        }
+        return result;
+    }
+
+
+
+
+
+
 }
 
